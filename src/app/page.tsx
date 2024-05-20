2610d0be226dcc1dc32f929cdeaca6692af1f165
ally. <79243058+wiredlime@users.noreply.github.com>
2024-05-20 21:34:12 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,112 +2,100 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+    <main className="relative grid min-h-screen  place-items-center p-24">
+      <div className="relative ">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
+          src="/linhmap.svg"
+          alt="Linh Map Logo"
+          width={300}
+          height={50}
           priority
         />
       </div>
+      <footer className="absolute bottom-5 left-5 flex items-center gap-2">
+        <div className="flex items-center gap-2">
+          <div className="relative w-5 h-5">
+            <Image
+              // className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+              src="/fb.svg"
+              alt="Facebook Logo"
+              fill
+              priority
+            />
+          </div>
+          <a
+            href="https://www.facebook.com/espresxobar?mibextid=LQQJ4d"
+            className="text-xs text-muted-background underline"
+          >
+            Expresobar
+          </a>
+        </div>
+        <div className="flex items-center gap-2 ">
+          <div className="relative w-5 h-5">
+            <Image
+              // className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+              src="/tt.svg"
+              alt="Tiktok Logo"
+              fill
+              priority
+            />
+          </div>
+          <a
+            href="https://www.tiktok.com/@espresxo.bar?fbclid=PAZXh0bgNhZW0CMTEAAabIzhOAO3tgMlhBWfjzrJLfK4zFp33iC1o6b3UzlwiZZwaJJFZnjSbbCGQ_aem_AWzc45Q_RoBA6pCQR8sO9phVXjHcnuwYef4_BCKphgTdoKdJKRFBPSUAaEPk1rUgJ1au8KoqmyqEJ5CSga4Y4M5G"
+            className="text-xs text-muted-background underline"
+          >
+            Expresobar
+          </a>
+        </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <div className="flex items-center gap-2">
+          <div className="relative w-5 h-5">
+            <Image
+              // className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+              src="/gg.svg"
+              alt="Google Logo"
+              fill
+              priority
+            />
+          </div>
+          <a
+            href="https://www.google.com/maps/place/EspresXO+BAR,+59+%C4%90.+Nguy%E1%BB%85n+Th%E1%BB%8B+Minh+Khai,+Ph%C6%B0%E1%BB%9Dng+B%E1%BA%BFn+Th%C3%A0nh,+Qu%E1%BA%ADn+1,+Th%C3%A0nh+ph%E1%BB%91+H%E1%BB%93+Ch%C3%AD+Minh+700000,+Vietnam/@10.7744791,106.690378,16z/data=!4m6!3m5!1s0x31752fcc66da419f:0x7d681a34bac00d09!8m2!3d10.7744791!4d106.690378!16s%2Fg%2F11vyr_77q6"
+            className="text-xs text-muted-background underline"
+          >
+            Expresobar
+          </a>
+        </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <div className="flex items-center gap-2">
+          <div className="relative w-5 h-5">
+            <Image src="/sp.svg" alt="Shopee Logo" fill priority className="" />
+          </div>
+          <a
+            href="https://shopeefood.vn/ho-chi-minh/espresxo-bar-nguyen-thi-minh-khai"
+            className="text-xs text-muted-background underline"
+          >
+            Expresobar
+          </a>
+        </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <div className="flex items-center gap-2">
+          <div className="relative w-5 h-5">
+            <Image
+              src="/ig.svg"
+              alt="Instagram Logo"
+              fill
+              priority
+              className=""
+            />
+          </div>
+          <a
+            href="https://www.instagram.com/espresxobar"
+            className="text-xs text-muted-background underline"
+          >
+            Expresobar
+          </a>
+        </div>
+      </footer>
     </main>
   );
 }
